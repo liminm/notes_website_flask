6c39f05cc1306513a5bd929fdbb29a4f59c7a28f
Limin <limin.malek@campus.tu-berlin.de>
2022-11-22 00:56:27 +0100

    Tweaks

--- website/__init__.py
+++ website/__init__.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+#DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +11,7 @@ def create_app():
     # local database
     #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rxmmguileuwnza:f938dc8d8fe0df10d9030a14af700efbde229e48329f151d7b6e801dabb848b9@ec2-52-71-23-11.compute-1.amazonaws.com:5432/d7847n1d0acbgm'
+    db = SQLAlchemy(app)
 
 
     db.init_app(app)
